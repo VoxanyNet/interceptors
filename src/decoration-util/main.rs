2a10c327efb3788dade746c2b9d0686ecf5c60f6
Kruz <kruzerbroek@gmail.com>
2025-12-24 13:02:17 -0500

    buncha new decorations

--- src/decoration-util/main.rs
+++ src/decoration-util/main.rs
@@ -20,14 +20,25 @@ struct Args {
     scale: Option<f32>,
 }
 
-fn error_print(message: String) {
-    eprintln!("{}", message);
+fn wait_for_user_enter() {
+     // pause execution
+    let mut string_input = String::new();
+    io::stdin()
+        .read_line(&mut string_input)
+        .expect("Failed to read line");
+}
 
-    std::thread::sleep(Duration::from_secs(10));
+fn error_print(message: String) {
+    log::error!("{}", message);
+
+   wait_for_user_enter();
 
 }
 
 fn main() {
+
+    pretty_env_logger::init();
+
     let args = match Args::try_parse() {
         Ok(args) => args,
         Err(error) => {
@@ -58,17 +69,20 @@ fn main() {
         },
     };
 
-    error_print(scale.to_string());
+    log::info!("Converting {} assets to prefabs with scaling of {}", args.asset_paths.len(), scale);
 
     set_current_dir(env::current_exe().unwrap().parent().unwrap()).unwrap();
     
     for asset_path in args.asset_paths {
         asset_to_decoration_prefab(asset_path, scale);
     }
+
+    wait_for_user_enter();
     
 }
 
 fn asset_to_decoration_prefab(asset_path: String, scale: f32) {
+
     let img = match ImageReader::open(&asset_path) {
         Ok(image_reader) => {
             match image_reader.decode() {
@@ -87,15 +101,21 @@ fn asset_to_decoration_prefab(asset_path: String, scale: f32) {
         },
     };
 
+    
+
     let (width, height) = img.dimensions();
 
-    let relative_path = match amongus(asset_path) {
+    
+
+    let relative_path = match get_path_relative_to_assets_folder(asset_path) {
         Ok(relative_path) => relative_path,
         Err(error) => {
             error_print("Asset must exist in assets directory".to_string());
             return;
         },
     };
+
+    log::info!("Loaded {} with dimensions: {:?}", &relative_path.to_string_lossy(), img.dimensions());
 
     let decoration_save = DecorationSave {
         pos: Vec2::ZERO,
@@ -106,7 +126,10 @@ fn asset_to_decoration_prefab(asset_path: String, scale: f32) {
         layer: 0,
     };
 
+    
+
     let save = serde_json::to_string_pretty(&decoration_save).unwrap();
+
 
     let prefab_path = format!("prefabs/decorations/{}.json", &relative_path.file_stem().unwrap().to_string_lossy().to_string());
 
@@ -115,9 +138,11 @@ fn asset_to_decoration_prefab(asset_path: String, scale: f32) {
         Err(error) => error_print(format!("Error writing destination file '{}': {}", &prefab_path, error)),
     }
 
+    log::info!("Successfully wrote prefab to: {}", prefab_path);
+
 }
 
-fn amongus(asset_path: String) -> Result<PathBuf, ()> {
+fn get_path_relative_to_assets_folder(asset_path: String) -> Result<PathBuf, ()> {
     // get the path relative to assets
     let path = PathBuf::from(asset_path);
 
